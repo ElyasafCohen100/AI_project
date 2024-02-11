7885cdb7af1430efb9191ee999f968c64f1f8250
Daniel Tsirkin <danieltsirkin2@gmail.com>
2024-02-11 22:20:21 +0200

    distance funcs and face vectors

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import cv2
 import dlib
+import math
 
 # Load the pre-trained facial landmark detector
 predictor_path = "shape_predictor_68_face_landmarks.dat"
@@ -29,7 +30,38 @@ def detect_landmarks(image):
         analyzedFaces += [landmarksList]
     return analyzedFaces
 
+def generateVector(analyzedFaces):
+    facesVectors=[]
+    for face in analyzedFaces:
+        LeyebowL =distancePoint(face[37],face[18],'Y')
+        LeyebowM =distancePoint(face[37],face[19],'Y')
+        LeyebowR = distancePoint(face[38],face[20],'Y')
+        ReyebowL = distancePoint(face[25],face[44],'Y')
+        ReyebowM = distancePoint(face[24],face[44],'Y')
+        ReyebowR = distancePoint(face[23],face[43],'Y')
+        LeyeL= distancePoint(face[37],face[41],'Y')
+        LeyeR = distancePoint(face[38],face[40],'Y')
+        ReyeL =distancePoint(face[43],face[47],'Y')
+        ReyeR =distancePoint(face[44],face[46],'Y')
+        lengthMouseL = distancePoint(face[61],face[67],'Y')
+        lengthMouseM = distancePoint(face[62], face[66],'Y')
+        lengthMouseR = distancePoint(face[63], face[65],'Y')
+        widthMouse = distancePoint(face[54],face[48],'X')
+        lengthUnit = distancePoint(face[0],face[1],'Y')
+        widthUnit = distancePoint(face[0], face[16], 'X')
+        facesVectors += [[LeyebowL/lengthUnit,LeyebowM/lengthUnit,LeyebowR/lengthUnit,ReyebowL/lengthUnit,ReyebowM/lengthUnit,ReyebowR/lengthUnit,LeyeL/lengthUnit,LeyeR/lengthUnit,ReyeL/lengthUnit,ReyeR/lengthUnit,lengthMouseL/lengthUnit,lengthMouseM/lengthUnit,lengthMouseR/lengthUnit,widthMouse/widthUnit]]
+    return facesVectors
 
+
+
+def distancePoint(dict1,dict2,str):
+    return math.fabs(dict2[str]-dict1[str])
+
+def vectorDistance(vectorImage1,vectorImage2):
+    sum=0
+    for i in range(14):
+        sum += math.pow(vectorImage1[i]-vectorImage2[i],2)
+    return math.sqrt(sum)
 
 # Initialize webcam
 cap = cv2.VideoCapture(0)
